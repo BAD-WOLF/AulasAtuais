0ae5a3d83643a121b61605cd4ad648897440d67b
matheu <matheusviaira160@gmail.com>
2024-11-08 20:56:24 -0300

    Add: Entitys

---
+++ Enterprise/Entitys/OutsourcedEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace Enterprise.Entitys {
+
+    internal sealed class OutsourcedEmployee(String name, Int32 hour, Single valuePerHour, Single additionalCharge) : Employee(name, hour, valuePerHour) {
+        private Single _additionalCharge = additionalCharge;
+
+        public Single AdditionalCharge { get => this._additionalCharge; set => this._additionalCharge = value; }
+
+        public override Single Payment() {
+            Single value = base.Payment();
+            return (this.AdditionalCharge * (110 / 100)) + value;
+        }
+    }
+}
