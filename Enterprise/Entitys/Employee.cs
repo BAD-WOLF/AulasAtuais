92dc9a24a28aa09de399c064ef933dfa652bd159
matheu <matheusviaira160@gmail.com>
2024-11-08 20:56:24 -0300

    Add: Entitys

---
+++ Enterprise/Entitys/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Enterprise.Entitys {
+    using System;
+
+    internal class Employee {
+        private String _name;
+        private Int32 _hour;
+        private Single _valuePerHour;
+
+        public Employee(String name, Int32 hour, Single valuePerHour) {
+            this._name = name;
+            this._hour = hour;
+            this._valuePerHour = valuePerHour;
+        }
+
+        public String Name { get => this._name; set => this._name = value; }
+        public Int32 Hour { get => this._hour; set => this._hour = value; }
+        public Single ValuePerHour { get => this._valuePerHour; set => this._valuePerHour = value; }
+
+        public virtual Single Payment() {
+            return this.ValuePerHour * this.Hour;
+        }
+    }
+}
---
+++ Enterprise/Entitys/OutsourcedEmployee.cs
@@ -0,0 +1,13 @@
+﻿namespace Enterprise.Entitys {
+
+    internal sealed class OutsourcedEmployee(String name, Int32 hour, Single valuePerHour, Int32 additionalCharge) : Employee(name, hour, valuePerHour) {
+        private Int32 _additionalCharge = additionalCharge;
+
+        public Int32 AdditionalCharge { get => this._additionalCharge; set => this._additionalCharge = value; }
+
+        public override Single Payment() {
+            Single value = base.Payment();
+            return value * (this.AdditionalCharge / 100);
+        }
+    }
+}
